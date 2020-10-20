cb4654665d5e271eb7f7c04f47a3238273022c2d
DarrenKwonDev <anatomy1545@gmail.com>
2020-10-20 17:20:58 +0900

    verification graphql type and entity

---
+++ uber-server/src/entities/Verification.ts
@@ -0,0 +1,32 @@
+import { verificationTarget } from "src/types/types";
+import {
+  BaseEntity,
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from "typeorm";
+
+@Entity()
+class Verification extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: "text", enum: ["PHONE", "EMAIL"] })
+  target: verificationTarget;
+
+  @Column({ type: "text" })
+  paylaod: string;
+
+  @Column({ type: "text" })
+  key: string;
+
+  @Column({ type: "boolean", default: false })
+  used: boolean;
+
+  @CreateDateColumn() createdAt: string;
+  @UpdateDateColumn() updatedAt: string;
+}
+
+export default Verification;
--- uber-server/src/types/graph.d.ts
+++ uber-server/src/types/graph.d.ts
@@ -1,4 +1,4 @@
-export const typeDefs = ["type Query {\n  sayBye: String!\n  sayHello(name: String!): HelloResponse!\n  user: User\n}\n\ntype HelloResponse {\n  text: String!\n  error: Boolean!\n}\n\ntype User {\n  id: Int!\n  email: String\n  verifiedEmail: Boolean!\n  firstName: String!\n  lastName: String!\n  age: Int\n  password: String\n  phoneNumber: String\n  verifiedPhonenNumber: Boolean!\n  profilePhoto: String\n  fullName: String\n  isDriving: Boolean!\n  isRiding: Boolean!\n  isTaken: Boolean!\n  lastLng: Float\n  lastLat: Float\n  lastOrientation: Float\n  createdAt: String!\n  updatedAt: String\n}\n"];
+export const typeDefs = ["type Query {\n  sayBye: String!\n  sayHello(name: String!): HelloResponse!\n  user: User\n}\n\ntype HelloResponse {\n  text: String!\n  error: Boolean!\n}\n\ntype User {\n  id: Int!\n  email: String\n  verifiedEmail: Boolean!\n  firstName: String!\n  lastName: String!\n  age: Int\n  password: String\n  phoneNumber: String\n  verifiedPhonenNumber: Boolean!\n  profilePhoto: String\n  fullName: String\n  isDriving: Boolean!\n  isRiding: Boolean!\n  isTaken: Boolean!\n  lastLng: Float\n  lastLat: Float\n  lastOrientation: Float\n  createdAt: String!\n  updatedAt: String\n}\n\ntype Verification {\n  id: Int!\n  target: String!\n  paylaod: String!\n  key: String!\n  used: Boolean!\n  createdAt: String!\n  updatedAt: String!\n}\n"];
 /* tslint:disable */
 
 export interface Query {
@@ -36,4 +36,14 @@ export interface User {
   lastOrientation: number | null;
   createdAt: string;
   updatedAt: string | null;
+}
+
+export interface Verification {
+  id: number;
+  target: string;
+  paylaod: string;
+  key: string;
+  used: boolean;
+  createdAt: string;
+  updatedAt: string;
 }
